5d2a5ca6014578a550d86b01d098b16de4eed1b5
carriecarter <carrie@carrie-carter.com>
2018-06-25 16:12:43 -0700

    mapped test passing

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,4 +1,18 @@
 const assert = require('assert');
-const arrayMethods = require('../lib/array-methods');
+const { map } = require('../lib/array-methods');
 
-describe 
+describe('array methods', () => {
+
+
+    describe('map', () => {
+        
+        it('transforms an array', () => {
+            const arr = [1, 2, 3];
+            const mapArray = map(arr, (val) => {
+                return val + 1;
+            });
+            assert.deepEqual(mapArray, [2, 3, 4]);
+        });
+    });
+
+});
